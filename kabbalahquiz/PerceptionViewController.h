40f1f92a0d19b8252687f975a5623ec5c0c9967b
Gabe Morales <gabomorales@me.com>
2014-03-24 19:50:29 -0500

    added perception view and plist

---
+++ kabbalahquiz/PerceptionViewController.h
@@ -0,0 +1,13 @@
+//
+//  PerceptionViewController.h
+//  kabbalahquiz
+//
+//  Created by Rockstar. on 3/24/14.
+//  Copyright (c) 2014 Bnei Baruch USA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PerceptionViewController : UIViewController
+
+@end
